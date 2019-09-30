3572a1163541417f1e1be4376ec676301cbb21e0
rossmurr4y <ross.murray@protonmail.com>
2019-09-30 14:44:56 -0700

    created provider utility script for supplimenting the shared utility.sh with provider specific ones. Script may move locations very shortly but this establishes its creation.

--- utility.sh
+++ utility.sh
@@ -8,15 +8,16 @@ function az_create_pki_credentials() {
   local dir="$1"; shift
   local region="$1"; shift
   local account="$1"; shift
+  local keytype="$1"; shift
 
-  if [[ (! -f "${dir}/azure-ssh-crt.pem") &&
-        (! -f "${dir}/azure-ssh-prv.pem") &&
-        (! -f "${dir}/.azure-ssh-crt.pem") &&
-        (! -f "${dir}/.azure-ssh-prv.pem") &&
-        (! -f "${dir}/.azure-${account}-${region}-ssh-crt.pem") &&
-        (! -f "${dir}/.azure-${account}-${region}-ssh-prv.pem") ]]; then
-      openssl genrsa -out "${dir}/.azure-${account}-${region}-ssh-prv.pem.plaintext" 2048 || return $?
-      openssl rsa -in "${dir}/.azure-${account}-${region}-ssh-prv.pem.plaintext" -pubout > "${dir}/.azure-${account}-${region}-ssh-crt.pem" || return $?
+  if [[ (! -f "${dir}/azure-${keytype}-crt.pem") &&
+        (! -f "${dir}/azure-${keytype}-prv.pem") &&
+        (! -f "${dir}/.azure-${keytype}-crt.pem") &&
+        (! -f "${dir}/.azure-${keytype}-prv.pem") &&
+        (! -f "${dir}/.azure-${account}-${region}-${keytype}-crt.pem") &&
+        (! -f "${dir}/.azure-${account}-${region}-${keytype}-prv.pem") ]]; then
+      openssl genrsa -out "${dir}/.azure-${account}-${region}-${keytype}-prv.pem.plaintext" 2048 || return $?
+      openssl rsa -in "${dir}/.azure-${account}-${region}-${keytype}-prv.pem.plaintext" -pubout > "${dir}/.azure-${account}-${region}-${keytype}-crt.pem" || return $?
   fi
 
   if [[ ! -f "${dir}/.gitignore" ]]; then
@@ -34,46 +35,52 @@ function az_delete_pki_credentials() {
   local dir="$1"; shift
   local region="$1"; shift
   local account="$1"; shift
+  local keytype="$1"; shift
 
   local restore_nullglob="$(shopt -p nullglob)"
   shopt -s nullglob
 
-  rm -f "${dir}"/.azure-${account}-${region}-ssh-crt* "${dir}"/.azure-${account}-${region}-ssh-prv*
+  rm -f "${dir}"/.azure-${account}-${region}-${keytype}-crt* "${dir}"/.azure-${account}-${region}-${keytype}-prv*
 
   ${restore_nullglob}
 }
 
-# -- SSH --
+# -- Keys --
 
-function az_check_ssh_credentials() {
-  local region="$1"; shift
-  local keyId="$1"; shift
+function az_check_key_credentials() {
+  local vaultName="$1"; shift
+  local keyName="$1"; shift
+
+  local keyId="https://${vaultName}.azure.net/keys/${keyName}"
 
   az keyvault key show --id "${keyId}" 2>&1 > /dev/null
 }
 
-function az_show_ssh_credentials() {
-  local region="$1"; shift
-  local keyId="$1"; shift
+function az_show_key_credentials() {
+  local vaultName="$1"; shift
+  local keyName="$1"; shift
+
+  local keyId="https://${vaultName}.azure.net/keys/${keyName}"
 
   az keyvault key show --id "${keyId}"
 }
 
-function az_update_ssh_credentials() {
-  local region="$1"; shift
-  local vault="$1"; shift
-  local name="$1"; shift
+function az_update_key_credentials() {
+  local vaultName="$1"; shift
+  local keyName="$1"; shift
   local crt_file="$1"; shift
 
   local crt_content=$(dos2unix < "${crt_file}" | awk 'BEGIN {RS="\n"} /^[^-]/ {printf $1}')
   ${crt_content} > ${crt_file}
 
-  az keyvault key import --pem-file "${crt_file}" --vault-name "${vault}" --name "${name}"
+  az keyvault key import --pem-file "${crt_file}" --vault-name "${vaultName}" --name "${keyName}"
 }
 
-function az_delete_ssh_credentials() {
-  local region="$1"; shift
-  local keyId="$1"; shift
+function az_delete_key_credentials() {
+  local vaultName="$1"; shift
+  local keyName="$1"; shift
+
+  local keyId="https://${vaultName}.azure.net/keys/${keyName}"
 
   #azure returns a large object upon successful deletion, so we redirect that.
   az keyvault key show --id "${keyId}" 2>&1 > /dev/null && \
