e5cba2a04790f67455be8007c36e77b74b8ca0d9
Ross Murray <rossmurray@protonmail.com>
2020-06-21 10:41:44 +1000

    refactor(testcases): Only perform soln/app createTemplate if not exist
    Incl. check on each createTemplate to only perform the action if the
    previous one didn't create anything.

--- test/run_azure_template_tests.sh
+++ test/run_azure_template_tests.sh
@@ -4,7 +4,8 @@ echo "###############################################"
 echo "# Running template tests for the AZURE provider #"
 echo "###############################################"
 
-TEST_OUTPUT_DIR="${TEST_OUTPUT_DIR:-"./hamlet_tests"}"
+DEFAULT_TEST_OUTPUT_DIR="$(pwd)/hamlet_tests"
+TEST_OUTPUT_DIR="${TEST_OUTPUT_DIR:-${DEFAULT_TEST_OUTPUT_DIR}}"
 
 if [[ -d "${TEST_OUTPUT_DIR}" ]]; then
     rm -r "${TEST_OUTPUT_DIR}"
@@ -15,18 +16,19 @@ fi
 
 echo "Output Dir: ${TEST_OUTPUT_DIR}"
 echo "Generating unit list..."
-${GENERATION_DIR}/createTemplate.sh -i mock -p azure -p azuretest -f arm -r westus -o "${TEST_OUTPUT_DIR}" -l unitlist
+"${GENERATION_DIR}"/createTemplate.sh -i mock -p azure -p azuretest -f arm -r westus -o "${TEST_OUTPUT_DIR}" -l unitlist
 UNIT_LIST="$(jq -r '.DeploymentUnits | join(" ")' < "${TEST_OUTPUT_DIR}/unitlistconfig.json")"
 
 for unit in $UNIT_LIST; do
     echo "Creating templates for $unit ..."
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p azure -p azuretest -f arm -r westus -o "${TEST_OUTPUT_DIR}" -l segment -u $unit > /dev/null 2>&1 || true
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p azure -p azuretest -f arm -r westus -o "${TEST_OUTPUT_DIR}" -l solution -u $unit > /dev/null 2>&1 || true
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p azure -p azuretest -f arm -r westus -o "${TEST_OUTPUT_DIR}" -l application -u $unit > /dev/null 2>&1 || true
+    echo "output will be: ${TEST_OUTPUT_DIR} and unit is ${unit}"
+    echo "trying segment..." && "${GENERATION_DIR}"/createTemplate.sh -i mock -p azure -p azuretest -f arm -r westus -o "${TEST_OUTPUT_DIR}" -l segment -u "${unit}" > /dev/null 2>&1 || true
+    [[ ! -e "${TEST_OUTPUT_DIR}/*${unit}*-testcase.json" ]] && echo "trying solution..." && "${GENERATION_DIR}"/createTemplate.sh -i mock -p azure -p azuretest -f arm -r westus -o "${TEST_OUTPUT_DIR}" -l solution -u "${unit}" > /dev/null 2>&1 || true
+    [[ ! -e "${TEST_OUTPUT_DIR}/*${unit}*-testcase.json" ]] && echo "trying application..." && "${GENERATION_DIR}"/createTemplate.sh -i mock -p azure -p azuretest -f arm -r westus -o "${TEST_OUTPUT_DIR}" -l application -u "${unit}" > /dev/null 2>&1 || true
 done
 
 hamlet test generate --directory "${TEST_OUTPUT_DIR}" -o "${TEST_OUTPUT_DIR}/test_templates.py"
 
-cd "${TEST_OUTPUT_DIR}"
+cd "${TEST_OUTPUT_DIR}" || exit
 echo "Running Tests..."
 hamlet test run -t "./test_templates.py"
